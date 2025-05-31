864e8f640cdc94775af29115b2c4ed65c4326742
adithc <adith.chandraiah@gmail.com>
2025-05-31 14:38:28 -0500

    day 1

---
+++ include/dummy.h
@@ -0,0 +1,4 @@
+// include/dummy.h
+#pragma once
+
+// Placeholder. Actual headers (dataset.h, optimizer.h, importance_sampling.h) come later.
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// src/main.cpp
+#include <iostream>
+
+// (Later we’ll add real logic here for Distributed SGD + Importance Sampling.)
+
+int main() {
+    std::cout << "Distributed SGD starter: Hello, world!\n";
+    return 0;
+}
